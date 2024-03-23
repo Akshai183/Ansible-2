40dd7d1628a14d20dd27999fff19adcad34a7c39
Akshai <akshay05pakhanati@gmail.com>
2024-03-23 19:38:35 +0530

    Ansible Base Code

--- 9.vpc-peering.tf
+++ 9.vpc-peering.tf
@@ -26,16 +26,16 @@ resource "aws_vpc_peering_connection" "ansible-vpc-peering" {
 }
 
 resource "aws_route" "peering-to-ansible-vpc" {
-  route_table_id            = aws_route_table.terraform-public.id
-  destination_cidr_block    = data.aws_vpc.ansible_vpc.cidr_block
+  route_table_id         = aws_route_table.terraform-public.id
+  destination_cidr_block = data.aws_vpc.ansible_vpc.cidr_block
   #"10.40.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.ansible-vpc-peering.id
   #depends_on                = [aws_route_table.terraform-public]
 }
 
 resource "aws_route" "peering-from-ansible-vpc" {
-  route_table_id            = data.aws_route_table.ansible_vpc_rt.id
-  destination_cidr_block    = aws_vpc.default.cidr_block 
+  route_table_id         = data.aws_route_table.ansible_vpc_rt.id
+  destination_cidr_block = aws_vpc.default.cidr_block
   #"10.38.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.ansible-vpc-peering.id
   #depends_on                = [aws_route_table.terraform-public]
