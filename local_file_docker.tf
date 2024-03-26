042e37c1f04e1728e47b570343902163f787ce54
Akshai <akshay05pakhanati@gmail.com>
2024-03-26 19:31:53 +0530

    code for Ansible-Collections

--- local_file_docker.tf
+++ local_file_docker.tf
@@ -5,9 +5,7 @@ resource "local_file" "docker-inventory-file" {
       testserver01 = aws_instance.webservers.0.public_ip
       testserver02 = aws_instance.webservers.1.public_ip
       testserver03 = aws_instance.webservers.2.public_ip
-      testserver04 = aws_instance.webservers.3.public_ip
-      testserver05 = aws_instance.webservers.4.public_ip
-      testserver06 = aws_instance.webservers.5.public_ip
+
     }
   )
   filename = "dockerinvfile"
